8a0a56b27354d90d71cf1e80675098c709093cb2
mendel <mendel.urssu@gmail.com>
2022-10-29 21:31:27 +0900

    Initial commit

---
+++ src/KotlinTestStarter.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val solutionClass=Solution()
+    val answer=solutionClass.solution("hello alorithm")
+    println(answer)
+}
+
+class Solution{
+    fun solution(string: String):String{
+        return string
+    }
+}
